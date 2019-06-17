13210b80328bf5e0c4f30e0a00140beeeaa38b30
Mahesh Waghmare <maheshw@bsf.io>
2019-06-17 19:37:31 +0530

    Translated 7070 strings in Marathi language from 100 popular plugins.

---
+++ 100-popular-plugins.bat
@@ -0,0 +1,299 @@
+wp wpi18n download_plugin_po contact-form-7 --lang=mr
+wp wpi18n generate_plugin_po contact-form-7 --lang=mr
+
+wp wpi18n download_plugin_po wordpress-seo --lang=mr
+wp wpi18n generate_plugin_po wordpress-seo --lang=mr
+
+wp wpi18n download_plugin_po akismet --lang=mr
+wp wpi18n generate_plugin_po akismet --lang=mr
+
+wp wpi18n download_plugin_po jetpack --lang=mr
+wp wpi18n generate_plugin_po jetpack --lang=mr
+
+wp wpi18n download_plugin_po classic-editor --lang=mr
+wp wpi18n generate_plugin_po classic-editor --lang=mr
+
+wp wpi18n download_plugin_po woocommerce --lang=mr
+wp wpi18n generate_plugin_po woocommerce --lang=mr
+
+wp wpi18n download_plugin_po wordpress-importer --lang=mr
+wp wpi18n generate_plugin_po wordpress-importer --lang=mr
+
+wp wpi18n download_plugin_po wordfence --lang=mr
+wp wpi18n generate_plugin_po wordfence --lang=mr
+
+wp wpi18n download_plugin_po duplicate-post --lang=mr
+wp wpi18n generate_plugin_po duplicate-post --lang=mr
+
+wp wpi18n download_plugin_po tinymce-advanced --lang=mr
+wp wpi18n generate_plugin_po tinymce-advanced --lang=mr
+
+wp wpi18n download_plugin_po really-simple-ssl --lang=mr
+wp wpi18n generate_plugin_po really-simple-ssl --lang=mr
+
+wp wpi18n download_plugin_po all-in-one-seo-pack --lang=mr
+wp wpi18n generate_plugin_po all-in-one-seo-pack --lang=mr
+
+wp wpi18n download_plugin_po elementor --lang=mr
+wp wpi18n generate_plugin_po elementor --lang=mr
+
+wp wpi18n download_plugin_po google-sitemap-generator --lang=mr
+wp wpi18n generate_plugin_po google-sitemap-generator --lang=mr
+
+wp wpi18n download_plugin_po google-analytics-for-wordpress --lang=mr
+wp wpi18n generate_plugin_po google-analytics-for-wordpress --lang=mr
+
+wp wpi18n download_plugin_po all-in-one-wp-migration --lang=mr
+wp wpi18n generate_plugin_po all-in-one-wp-migration --lang=mr
+
+wp wpi18n download_plugin_po wpforms-lite --lang=mr
+wp wpi18n generate_plugin_po wpforms-lite --lang=mr
+
+wp wpi18n download_plugin_po updraftplus --lang=mr
+wp wpi18n generate_plugin_po updraftplus --lang=mr
+
+wp wpi18n download_plugin_po wp-super-cache --lang=mr
+wp wpi18n generate_plugin_po wp-super-cache --lang=mr
+
+wp wpi18n download_plugin_po google-analytics-dashboard-for-wp --lang=mr
+wp wpi18n generate_plugin_po google-analytics-dashboard-for-wp --lang=mr
+
+wp wpi18n download_plugin_po regenerate-thumbnails --lang=mr
+wp wpi18n generate_plugin_po regenerate-thumbnails --lang=mr
+
+wp wpi18n download_plugin_po mailchimp-for-wp --lang=mr
+wp wpi18n generate_plugin_po mailchimp-for-wp --lang=mr
+
+wp wpi18n download_plugin_po advanced-custom-fields --lang=mr
+wp wpi18n generate_plugin_po advanced-custom-fields --lang=mr
+
+wp wpi18n download_plugin_po limit-login-attempts --lang=mr
+wp wpi18n generate_plugin_po limit-login-attempts --lang=mr
+
+wp wpi18n download_plugin_po wp-smushit --lang=mr
+wp wpi18n generate_plugin_po wp-smushit --lang=mr
+
+wp wpi18n download_plugin_po siteorigin-panels --lang=mr
+wp wpi18n generate_plugin_po siteorigin-panels --lang=mr
+
+wp wpi18n download_plugin_po duplicator --lang=mr
+wp wpi18n generate_plugin_po duplicator --lang=mr
+
+wp wpi18n download_plugin_po redirection --lang=mr
+wp wpi18n generate_plugin_po redirection --lang=mr
+
+wp wpi18n download_plugin_po w3-total-cache --lang=mr
+wp wpi18n generate_plugin_po w3-total-cache --lang=mr
+
+wp wpi18n download_plugin_po wp-mail-smtp --lang=mr
+wp wpi18n generate_plugin_po wp-mail-smtp --lang=mr
+
+wp wpi18n download_plugin_po ninja-forms --lang=mr
+wp wpi18n generate_plugin_po ninja-forms --lang=mr
+
+wp wpi18n download_plugin_po disable-comments --lang=mr
+wp wpi18n generate_plugin_po disable-comments --lang=mr
+
+wp wpi18n download_plugin_po so-widgets-bundle --lang=mr
+wp wpi18n generate_plugin_po so-widgets-bundle --lang=mr
+
+wp wpi18n download_plugin_po cookie-notice --lang=mr
+wp wpi18n generate_plugin_po cookie-notice --lang=mr
+
+wp wpi18n download_plugin_po hello-dolly --lang=mr
+wp wpi18n generate_plugin_po hello-dolly --lang=mr
+
+wp wpi18n download_plugin_po wp-multibyte-patch --lang=mr
+wp wpi18n generate_plugin_po wp-multibyte-patch --lang=mr
+
+wp wpi18n download_plugin_po autoptimize --lang=mr
+wp wpi18n generate_plugin_po autoptimize --lang=mr
+
+wp wpi18n download_plugin_po wp-fastest-cache --lang=mr
+wp wpi18n generate_plugin_po wp-fastest-cache --lang=mr
+
+wp wpi18n download_plugin_po better-wp-security --lang=mr
+wp wpi18n generate_plugin_po better-wp-security --lang=mr
+
+wp wpi18n download_plugin_po limit-login-attempts-reloaded --lang=mr
+wp wpi18n generate_plugin_po limit-login-attempts-reloaded --lang=mr
+
+wp wpi18n download_plugin_po duplicate-page --lang=mr
+wp wpi18n generate_plugin_po duplicate-page --lang=mr
+
+wp wpi18n download_plugin_po coming-soon --lang=mr
+wp wpi18n generate_plugin_po coming-soon --lang=mr
+
+wp wpi18n download_plugin_po loco-translate --lang=mr
+wp wpi18n generate_plugin_po loco-translate --lang=mr
+
+wp wpi18n download_plugin_po nextgen-gallery --lang=mr
+wp wpi18n generate_plugin_po nextgen-gallery --lang=mr
+
+wp wpi18n download_plugin_po really-simple-captcha --lang=mr
+wp wpi18n generate_plugin_po really-simple-captcha --lang=mr
+
+wp wpi18n download_plugin_po optinmonster --lang=mr
+wp wpi18n generate_plugin_po optinmonster --lang=mr
+
+wp wpi18n download_plugin_po ml-slider --lang=mr
+wp wpi18n generate_plugin_po ml-slider --lang=mr
+
+wp wpi18n download_plugin_po loginizer --lang=mr
+wp wpi18n generate_plugin_po loginizer --lang=mr
+
+wp wpi18n download_plugin_po instagram-feed --lang=mr
+wp wpi18n generate_plugin_po instagram-feed --lang=mr
+
+wp wpi18n download_plugin_po worker --lang=mr
+wp wpi18n generate_plugin_po worker --lang=mr
+
+wp wpi18n download_plugin_po woocommerce-gateway-paypal-express-checkout --lang=mr
+wp wpi18n generate_plugin_po woocommerce-gateway-paypal-express-checkout --lang=mr
+
+wp wpi18n download_plugin_po wp-optimize --lang=mr
+wp wpi18n generate_plugin_po wp-optimize --lang=mr
+
+wp wpi18n download_plugin_po redux-framework --lang=mr
+wp wpi18n generate_plugin_po redux-framework --lang=mr
+
+wp wpi18n download_plugin_po wp-pagenavi --lang=mr
+wp wpi18n generate_plugin_po wp-pagenavi --lang=mr
+
+wp wpi18n download_plugin_po shortcodes-ultimate --lang=mr
+wp wpi18n generate_plugin_po shortcodes-ultimate --lang=mr
+
+wp wpi18n download_plugin_po breadcrumb-navxt --lang=mr
+wp wpi18n generate_plugin_po breadcrumb-navxt --lang=mr
+
+wp wpi18n download_plugin_po all-in-one-wp-security-and-firewall --lang=mr
+wp wpi18n generate_plugin_po all-in-one-wp-security-and-firewall --lang=mr
+
+wp wpi18n download_plugin_po tablepress --lang=mr
+wp wpi18n generate_plugin_po tablepress --lang=mr
+
+wp wpi18n download_plugin_po black-studio-tinymce-widget --lang=mr
+wp wpi18n generate_plugin_po black-studio-tinymce-widget --lang=mr
+
+wp wpi18n download_plugin_po insert-headers-and-footers --lang=mr
+wp wpi18n generate_plugin_po insert-headers-and-footers --lang=mr
+
+wp wpi18n download_plugin_po the-events-calendar --lang=mr
+wp wpi18n generate_plugin_po the-events-calendar --lang=mr
+
+wp wpi18n download_plugin_po better-search-replace --lang=mr
+wp wpi18n generate_plugin_po better-search-replace --lang=mr
+
+wp wpi18n download_plugin_po ewww-image-optimizer --lang=mr
+wp wpi18n generate_plugin_po ewww-image-optimizer --lang=mr
+
+wp wpi18n download_plugin_po broken-link-checker --lang=mr
+wp wpi18n generate_plugin_po broken-link-checker --lang=mr
+
+wp wpi18n download_plugin_po one-click-demo-import --lang=mr
+wp wpi18n generate_plugin_po one-click-demo-import --lang=mr
+
+wp wpi18n download_plugin_po custom-post-type-ui --lang=mr
+wp wpi18n generate_plugin_po custom-post-type-ui --lang=mr
+
+wp wpi18n download_plugin_po woocommerce-services --lang=mr
+wp wpi18n generate_plugin_po woocommerce-services --lang=mr
+
+wp wpi18n download_plugin_po yith-woocommerce-wishlist --lang=mr
+wp wpi18n generate_plugin_po yith-woocommerce-wishlist --lang=mr
+
+wp wpi18n download_plugin_po wp-maintenance-mode --lang=mr
+wp wpi18n generate_plugin_po wp-maintenance-mode --lang=mr
+
+wp wpi18n download_plugin_po backwpup --lang=mr
+wp wpi18n generate_plugin_po backwpup --lang=mr
+
+wp wpi18n download_plugin_po litespeed-cache --lang=mr
+wp wpi18n generate_plugin_po litespeed-cache --lang=mr
+
+wp wpi18n download_plugin_po force-regenerate-thumbnails --lang=mr
+wp wpi18n generate_plugin_po force-regenerate-thumbnails --lang=mr
+
+wp wpi18n download_plugin_po sg-cachepress --lang=mr
+wp wpi18n generate_plugin_po sg-cachepress --lang=mr
+
+wp wpi18n download_plugin_po contact-widgets --lang=mr
+wp wpi18n generate_plugin_po contact-widgets --lang=mr
+
+wp wpi18n download_plugin_po user-role-editor --lang=mr
+wp wpi18n generate_plugin_po user-role-editor --lang=mr
+
+wp wpi18n download_plugin_po sucuri-scanner --lang=mr
+wp wpi18n generate_plugin_po sucuri-scanner --lang=mr
+
+wp wpi18n download_plugin_po mailchimp-for-woocommerce --lang=mr
+wp wpi18n generate_plugin_po mailchimp-for-woocommerce --lang=mr
+
+wp wpi18n download_plugin_po post-types-order --lang=mr
+wp wpi18n generate_plugin_po post-types-order --lang=mr
+
+wp wpi18n download_plugin_po add-to-any --lang=mr
+wp wpi18n generate_plugin_po add-to-any --lang=mr
+
+wp wpi18n download_plugin_po woocommerce-gateway-stripe --lang=mr
+wp wpi18n generate_plugin_po woocommerce-gateway-stripe --lang=mr
+
+wp wpi18n download_plugin_po cookie-law-info --lang=mr
+wp wpi18n generate_plugin_po cookie-law-info --lang=mr
+
+wp wpi18n download_plugin_po beaver-builder-lite-version --lang=mr
+wp wpi18n generate_plugin_po beaver-builder-lite-version --lang=mr
+
+wp wpi18n download_plugin_po wp-statistics --lang=mr
+wp wpi18n generate_plugin_po wp-statistics --lang=mr
+
+wp wpi18n download_plugin_po googleanalytics --lang=mr
+wp wpi18n generate_plugin_po googleanalytics --lang=mr
+
+wp wpi18n download_plugin_po flamingo --lang=mr
+wp wpi18n generate_plugin_po flamingo --lang=mr
+
+wp wpi18n download_plugin_po polylang --lang=mr
+wp wpi18n generate_plugin_po polylang --lang=mr
+
+wp wpi18n download_plugin_po wp-google-maps --lang=mr
+wp wpi18n generate_plugin_po wp-google-maps --lang=mr
+
+wp wpi18n download_plugin_po antispam-bee --lang=mr
+wp wpi18n generate_plugin_po antispam-bee --lang=mr
+
+wp wpi18n download_plugin_po smart-slider-3 --lang=mr
+wp wpi18n generate_plugin_po smart-slider-3 --lang=mr
+
+wp wpi18n download_plugin_po wps-hide-login --lang=mr
+wp wpi18n generate_plugin_po wps-hide-login --lang=mr
+
+wp wpi18n download_plugin_po maintenance --lang=mr
+wp wpi18n generate_plugin_po maintenance --lang=mr
+
+wp wpi18n download_plugin_po woo-gutenberg-products-block --lang=mr
+wp wpi18n generate_plugin_po woo-gutenberg-products-block --lang=mr
+
+wp wpi18n download_plugin_po ocean-extra --lang=mr
+wp wpi18n generate_plugin_po ocean-extra --lang=mr
+
+wp wpi18n download_plugin_po meta-box --lang=mr
+wp wpi18n generate_plugin_po meta-box --lang=mr
+
+wp wpi18n download_plugin_po taxonomy-terms-order --lang=mr
+wp wpi18n generate_plugin_po taxonomy-terms-order --lang=mr
+
+wp wpi18n download_plugin_po easy-wp-smtp --lang=mr
+wp wpi18n generate_plugin_po easy-wp-smtp --lang=mr
+
+wp wpi18n download_plugin_po amp --lang=mr
+wp wpi18n generate_plugin_po amp --lang=mr
+
+wp wpi18n download_plugin_po wp-file-manager --lang=mr
+wp wpi18n generate_plugin_po wp-file-manager --lang=mr
+
+wp wpi18n download_plugin_po image-widget --lang=mr
+wp wpi18n generate_plugin_po image-widget --lang=mr
+
+wp wpi18n download_plugin_po velvet-blues-update-urls --lang=mr
+wp wpi18n generate_plugin_po velvet-blues-update-urls --lang=mr
